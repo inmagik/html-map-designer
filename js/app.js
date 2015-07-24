d2467bbdf4974491d6d42cf8320a893ccc891e4b
Mauro Bianchi <bianchimro@gmail.com>
2015-07-24 18:34:46 +0200

    layer sorting and editing

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
 (function(){
   "use strict";
 
-  angular.module("HtmlMap", ['angularModalService', 'ui.ace'])
-  .config(function($locationProvider){
+  angular.module("HtmlMap", ['angularModalService', 'ui.ace', 'ui.router', 'ui.bootstrap', 'ui.sortable'])
+  .config(function($locationProvider, $stateProvider, $urlRouterProvider){
     /*
     $locationProvider.html5Mode(
       { enabled: true
       }
     );
     */
+
+    $urlRouterProvider.otherwise("/start");
+    $stateProvider
+    .state('start', {
+      url: "/start",
+      templateUrl: "templates/start.html",
+      controller : 'StartCtrl'
+    })
+    .state('map-editor', {
+      url: "/map-editor",
+      templateUrl: "templates/map-editor.html",
+      controller : 'MapCtrl'
+    })
 
   })
   .run(function(){
--- js/controllers.js
+++ js/controllers.js
@@ -2,10 +2,61 @@
   "use strict";
 
   angular.module("HtmlMap")
-  .controller('BodyCtrl', function($scope, $timeout, ConfigService, MapsControllerDelegate, ModalService, $location, $rootScope){
+
+  .controller('BodyCtrl', function($scope,  $rootScope){
+
+    $rootScope.ui = {
+      mainViewClass : 'black'
+    }
+  })
+
+
+  .controller('StartCtrl', function($scope,  $rootScope, $timeout){
+
+    $timeout(function(){
+      $rootScope.ui.mainViewClass = 'mantle';
+    })
+
+
+
+  })
+
+  .controller('LoadCtrl', function($scope,  $rootScope, $timeout){
+    $rootScope.ui.mainViewClass = 'cherry';
+
+
+
+  })
+
+
+
+
+
+
+  .controller('MapCtrl', function($scope, $timeout, ConfigService, MapsControllerDelegate, ModalService, $location, $rootScope, $q, $modal){
 
     $scope.data = { config : null, configString : null, mapConfigError:false, geoStyleError:false };
     $scope.ui = { autoZoom : true };
+
+    function fixWidthHelper(e, ui) {
+      ui.children().each(function() {
+          $(this).width($(this).width());
+      });
+      return ui;
+    }
+    $scope.sortableOptions = {
+      helper: fixWidthHelper,
+      update: function(e, ui) {
+        console.log(e, ui);
+        updateCfg().then($scope.updateMap)
+      }
+    };
+
+    $scope.spliceLayer = function(l, pos){
+      $scope.data.config.mapConfig.layers.splice(pos, 1);
+      updateCfg().then($scope.updateMap)
+
+    }
 
     var s = $location.search();
 
@@ -37,14 +88,18 @@
     };
 
     var updateCfg = function(){
+      var deferred = $q.defer();
       $timeout(function(){
 
         $scope.data.configString = {
             mapConfig : JSON.stringify($scope.data.config.mapConfig, null, 4),
             geoStyle : $scope.data.config.geoStyle
         };
+        deferred.resolve(true);
 
       });
+
+      return deferred.promise;
 
 
     }
@@ -128,10 +183,63 @@
 
 
       });
+    };
 
+
+
+
+
+
+
+    $scope.openLayerModal = function (layerType, cfg) {
+
+      
+      cfg = cfg || { newLayer:true};
+      cfg.layerType = layerType;
+
+      var modalInstance = $modal.open({
+        templateUrl: "templates/layer-modal-"+layerType+".html",
+        controller: 'ModalInstanceCtrl',
+        resolve: {
+          cfg: function () {
+            return cfg;
+          }
+        }
+      });
+
+      modalInstance.result.then(function (ocfg) {
+        if(cfg.newLayer){
+          delete cfg.newLayer;
+          $scope.data.config.mapConfig.layers.push(cfg);  
+        }
+        updateCfg().then($scope.updateMap);
+      
+      });
 
     }
 
-  });
+})
+
+
+
+
+  .controller('ModalInstanceCtrl', function ($scope, $modalInstance, cfg) {
+
+    $scope.cfg = cfg;
+
+  
+    $scope.ok = function () {
+      $modalInstance.close($scope.cfg);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+
+});
+
+
+
+
 
 })();
